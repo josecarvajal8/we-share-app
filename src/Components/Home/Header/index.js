8b80428a482526ee53126b50c2b2cbe342b77de7
Jose Carvajal <josecarvajal@MacBook-Pro-de-Jose.local>
2018-08-19 23:10:28 -0500

    It was created the first approach header

--- src/Components/Home/Header/index.js
+++ src/Components/Home/Header/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import logo from '../../../logo.svg'
 import _ from '../../../Helpers/Utilities'
 import './style.css'
 export default () => {
     const tittle = _.capitalize('we share')
     return (
         <div>
-            <header className="App-header">
-                <h1 className="App-title">{tittle}</h1>
-            </header>
+            <div className="header">
+                <h1 className="title">{tittle}</h1>
+            </div>
         </div>
     )
 }
