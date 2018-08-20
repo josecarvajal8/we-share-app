ec0a269b87f73c76677768b93faa17115e851fef
Jose Carvajal <josecarvajal@MacBook-Pro-de-Jose.local>
2018-08-19 22:20:12 -0500

    Implementing struture for this project

---
+++ src/Components/Home/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../../../logo.svg'
+import './style.css'
+export default () => {
+    return (
+        <div>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to We Share</h1>
+            </header>
+        </div>
+    )
+}
