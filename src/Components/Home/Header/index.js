4ba56915be2901b01d2392ae9906fa8a13420dd6
Jose Carvajal <jose.carvajal@Jose-Carvajal.local>
2018-08-29 09:42:18 -0500

    navbar sections created, no actions implemented yet

--- src/Components/Home/Header/index.js
+++ src/Components/Home/Header/index.js
@@ -2,10 +2,15 @@ import React from 'react'
 import _ from '../../../Helpers/Utilities'
 import './style.css'
 export default ({ title }) => {
+    const btns = ['Home', 'About', 'Devs']
+    const listBtns = (data) => data.map(el => <div className='btns'><p className='textBtns'>{el}</p></div>)
     return (
         <div>
-            <div className="header">
-                <h1 className="title">{_.capitalize(title)}</h1>
+            <div className='header'>
+                <div className='containerList'>
+                    {listBtns(btns)}
+                </div>
+                <h1 className='title'>{_.capitalize(title)}</h1>
             </div>
         </div>
     )
